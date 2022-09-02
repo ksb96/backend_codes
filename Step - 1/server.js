62879c6ff9c6afd54d06613fe0fa52f092671a77
kubstar <52975077+kubstar@users.noreply.github.com>
2022-09-02 19:27:47 +0530

    step 1 -- commit

---
+++ Step - 1/server.js
@@ -0,0 +1,16 @@
+//creating a server + assigning port (statically)
+app.get('/', (req,res)=>{
+    res.send('Hello'); // when we get a req. from root(or a webpage), we give a response(to client) -> 'Hello'
+})
+app.listen(3000, ()=>console.log('Listening to port 3000'));
+
+
+
+//Assigning a port value (Dynamically)
+const port = process.env.PORT || 3000; //var created for port var
+app.listen(port, () =>
+    console.log(`Listening to ${port}`))
+
+// setting the port value
+// on cmd
+// set PORT=5000
