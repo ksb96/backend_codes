0a539ad5d0e13872bda091d8b9d06ec980dedcef
kubstar <52975077+kubstar@users.noreply.github.com>
2022-09-06 22:50:31 +0530

    ejs -- commit

---
+++ Ch - 8/server.js
@@ -0,0 +1,12 @@
+//Template engines(ejs) -- generatic dynamic 'html' & returning it to the client
+// on server.js file
+app.set('view engine', 'pug');
+app.set('views', './views'); //default dir(optional)
+
+//setting the values for the 'var's which are declared in ejs file(pug)
+app.get('/',(req,res)=>{
+    res.render('index',{
+        title:'My App',
+        messsage: 'Hi'
+    });
+});
