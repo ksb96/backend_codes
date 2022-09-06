49dd1c64ccc49c05aa5fe7fedd713cc4a9f2e954
kubstar <52975077+kubstar@users.noreply.github.com>
2022-09-06 19:48:28 +0530

    content updated -- commit

--- Ch - 7/middleware.js
+++ Ch - 7/middleware.js
@@ -1,7 +1,12 @@
 //middleware - a function which takes a req object, either gives a response to the client or passes control to another middleware
+//below codes should be kept in the 'server.js' file
 //in-build middleware
 app.get('/', (req,res) =>{
     res.send('Hi');
 });
 
 app.use(express.json()); //parse the 'req.body' at runtime
+
+
+//static file upload
+express.static('filename'); //'filename' - 'public/img/photo.png'
