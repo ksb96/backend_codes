03aafe9ba15c539b2be6dc81e57582d8e3f4a965
kubstar <52975077+kubstar@users.noreply.github.com>
2022-09-27 22:21:52 +0530

    auth endpoints -- commit

--- Ch - 11/auth.js
+++ Ch - 11/auth.js
@@ -5,5 +5,26 @@ const { Server } = require("http")
 
 // on Server.js
 const users = require('./routes/reg');
+const auth = require(); // ./routes/auth
 
 app.use('/api/users',users);
+app.use('/api/auth',auth);
+
+
+router.post('/', async(req,res)=>{
+    //auth
+    const { error } = validate(req.body);
+    if(error)
+    return res.status(400).send(error.details[0].message);
+    //email
+    let user = await User.findOne({ email: req.body.email });
+    if(!user)
+    return res.status(400).send('Invalid email or password');
+    //pswd
+    let validPassword = await bcrypt.compare(req.body.password, user.password );
+    if(!validPassword)
+    return res.status(400).send('Invalid email or password');
+
+    //valid login
+    res.send(true);
+});
