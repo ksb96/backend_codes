6f0fc79726848faf8eb0d81251e93328f0e30bbd
kubstar <52975077+kubstar@users.noreply.github.com>
2022-12-05 13:54:02 +0530

    event code -added

--- Notes/codes.js
+++ Notes/codes.js
@@ -22,3 +22,20 @@ function fun() {
     console.log('printing something');
 }
 module.exports.fun = fun; //module.exports(fun);
+
+
+
+//event
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var myEventHandler = function () {
+  console.log('I hear a scream!');
+}
+
+//Assign the eventhandler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
