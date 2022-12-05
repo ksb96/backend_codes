1b571f011ab3a1fb7ebb96e1a5df8ca129b8ddcd
kubstar <52975077+kubstar@users.noreply.github.com>
2022-12-05 13:35:10 +0530

    fs mod code -added

--- Ch - 2/server.js
+++ Ch - 2/server.js
@@ -8,3 +8,20 @@ app.get('/api/courses/:id', (req,res)=>{ //the api which is getting hit
 app.get('localhost:5000/api/courses/2020/1/?sortBy=name', (req,res)=>{ //query related address
     res.send(req.query); //giving response to client with the exact data(over here is the 'courses' with year '2020' & sorted by name) which is requested
 });
+
+
+
+//read a file(local) - asyn + sync
+//async
+var fs = require('fs');
+
+fs.readFile('filename.txt', (err,data)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log('async data is - ' + data.toString());
+    }
+});
+//sync
+var data = fs.readFileSync('filename.txt');
+console.log('Sync data is - ' + data.toString());
